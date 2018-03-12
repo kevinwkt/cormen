1324fde1fc58c9f1d5053729a0cf2b5b14901b63
kevinwkt <kevinwkt97@outlook.com>
2018-03-12 16:47:55 -0600

    initial commit

---
+++ chapter31/b2d.py
@@ -0,0 +1,14 @@
+
+def b2d(binary,index):
+	if len(binary)==1:
+		if binary=='1':
+			return 2**index
+		else:
+			return 0
+
+	mid=int(len(binary)/2)
+	return b2d(binary[:mid],index+len(binary)-mid)+b2d(binary[mid:],index)
+
+testcase="11001110101"
+print(b2d(testcase,0))
+
